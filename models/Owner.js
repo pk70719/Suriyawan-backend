11d3a126a04fcdc8f05efd26a184fa30853c3dd5
pk70719 <pradeepseth707@gmail.com>
2025-06-19 12:44:12 +0530

    Update Owner.js

--- models/Owner.js
+++ models/Owner.js
@@ -1,41 +1,33 @@
 const mongoose = require("mongoose");
 
-const ownerSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true
-    },
-
-    password: {
-      type: String,
-      required: true
-    },
-
-    name: {
-      type: String,
-      default: "Admin"
-    },
-
-    role: {
-      type: String,
-      default: "owner"
-    },
-
-    imageUrl: {
-      type: String,
-      default: ""
-    },
-
-    mobile: {
-      type: String,
-      default: ""
-    }
+const ownerSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    default: ""
+  },
+  name: {
+    type: String,
+    default: "Admin"
+  },
+  role: {
+    type: String,
+    default: "owner"
+  },
+  imageUrl: {
+    type: String,
+    default: ""
+  },
+  mobile: {
+    type: String,
+    default: ""
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Owner", ownerSchema);
